6790255f4b633f70fcf01f77cfeca06833e76f16
Zyro9922 <alihasan9922@gmail.com>
2019-01-05 16:01:13 +0530

    First

---
+++ app/src/main/java/com/example/alihasan/photoactivity/PhotoActivity.java
@@ -0,0 +1,298 @@
+package com.example.alihasan.photoactivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+
+
+import android.util.Base64;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+public class PhotoActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    FloatingActionButton floatingActionButton;
+    Button exitButton;
+    EditText ed;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private static final String FILE_PROVIDER_AUTHORITY = "com.example.alihasan.photoactivity.fileprovider";
+
+    private String mTempPhotoPath;
+
+    private Bitmap mResultsBitmap;
+
+    File photoFile = null;
+
+    static String globalImageFileName;
+
+    private int REQUEST_STRING_CODE=1234;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ed = findViewById(R.id.ed)
+;
+        /**
+         * PERMISSION CHECKS
+         */
+
+        String []permissionsList={Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE};
+        ActivityCompat.requestPermissions(this,
+                permissionsList,
+                REQUEST_STRING_CODE);
+
+
+        imageView = findViewById(R.id.imageView);
+        floatingActionButton = findViewById(R.id.photoButton);
+        exitButton = findViewById(R.id.exitButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(PhotoActivity.this,Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED ||
+                        ContextCompat.checkSelfPermission(PhotoActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED) {
+
+                    exitButton.setVisibility(View.VISIBLE);
+                    launchCamera();
+                } else {
+                    Toast.makeText(getApplicationContext(), "GRANT STORAGE & CAMERA PERMISSION", Toast.LENGTH_SHORT).show();
+
+                    String []permissionsList={Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE};
+                    ActivityCompat.requestPermissions(PhotoActivity.this,
+                            permissionsList,
+                            REQUEST_STRING_CODE);
+
+                }
+            }
+        });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Blob...", Toast.LENGTH_SHORT).show();
+
+                final Intent emailIntent = new Intent( android.content.Intent.ACTION_SEND);
+
+                emailIntent.setType("plain/text");
+
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
+                        ""+base64conversion(photoFile));
+
+                startActivity(Intent.createChooser(
+                        emailIntent, "Send mail..."));
+            }
+        });
+
+    }
+
+    private void launchCamera() {
+
+        // Create the capture image intent
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the temporary File where the photo should go
+            try {
+                photoFile = createTempImageFile(PhotoActivity.this);
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+
+                // Get the path of the temporary file
+                mTempPhotoPath = photoFile.getAbsolutePath();
+
+                // Get the content URI for the image file
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        FILE_PROVIDER_AUTHORITY,
+                        photoFile);
+
+                // Add the URI so the camera can store the image
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                // Launch the camera activity
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // If the image capture activity was called and was successful
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Process the image and set it to the TextView
+            processAndSetImage();
+        } else {
+
+            // Otherwise, delete the temporary image file
+            deleteImageFile(this, mTempPhotoPath);
+        }
+    }
+
+    /**
+     * Method for processing the captured image and setting it to the TextView.
+     */
+    private void processAndSetImage() {
+
+        // Resample the saved image to fit the ImageView
+        mResultsBitmap = resamplePic(this, mTempPhotoPath);
+
+        Toast.makeText(getApplicationContext(), "Intent send = new Intent(Intent.ACTION_SEND);", Toast.LENGTH_SHORT).show();
+        // Set the new bitmap to the ImageView
+        imageView.setImageBitmap(mResultsBitmap);
+    }
+
+    /**
+     * Resamples the captured photo to fit the screen for better memory usage.
+     *
+     * @param context   The application context.
+     * @param imagePath The path of the photo to be resampled.
+     * @return The resampled bitmap
+     */
+    static Bitmap resamplePic(Context context, String imagePath) {
+
+        // Get device screen size information
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        manager.getDefaultDisplay().getMetrics(metrics);
+
+        int targetH = metrics.heightPixels;
+        int targetW = metrics.widthPixels;
+
+        // Get the dimensions of the original bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inScaled = false;
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeFile(imagePath);
+    }
+
+    /**
+     * Creates the temporary image file in the cache directory.
+     *
+     * @return The temporary image file.
+     * @throws IOException Thrown if there is an error creating the file
+     */
+    static File createTempImageFile(Context context)
+            throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        String imageFileName = "AZTEK" + timeStamp + "_";
+
+        globalImageFileName = imageFileName;
+
+        File storageDir = context.getExternalCacheDir();
+
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+    }
+
+    /**
+     * Deletes image file for a given path.
+     *
+     * @param context   The application context.
+     * @param imagePath The path of the photo to be deleted.
+     */
+    static boolean deleteImageFile(Context context, String imagePath) {
+
+        // Get the file
+        File imageFile = new File(imagePath);
+
+        // Delete the image
+        boolean deleted = imageFile.delete();
+
+        // If there is an error deleting the file, show a Toast
+        if (!deleted) {
+            String errorMessage = context.getString(R.string.error);
+
+        }
+
+        return deleted;
+    }
+
+    public String base64conversion(File destination)
+    {
+        try {
+            FileInputStream in = new FileInputStream(destination);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            //THE JUICE
+            options.inScaled = false;
+            Log.d("PP", " bitmap factory=========="+options);
+            Bitmap user_picture_bmp = BitmapFactory.decodeStream(in, null, options);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            user_picture_bmp.compress(Bitmap.CompressFormat.JPEG, 20, bos);
+            byte[] bArray = bos.toByteArray();
+            String encodedImage = Base64.encodeToString(bArray, Base64.DEFAULT);
+            return encodedImage;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "ERROR";
+        }
+    }
+
+
+}
+
+
